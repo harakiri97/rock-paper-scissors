c4ecde7aa9c508d5bb729e6815a55143439128ce
julius-zi <j97.zimmermann@gmx.de>
2023-06-15 21:25:40 +0200

    Add Javascript functionalities.

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 
 
 
-let yourScore = 0;
-let computerScore = 0;
+let myScore = 0;
+let compScore = 0;
+let gameOver = false;
 
-console.log(game());
+
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => { 
+    button.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        const playerSelection = button.innerText.toUpperCase();
+        const result = gambleOneRound(computerSelection,playerSelection);
+
+        game(result);
+    });
+});
 
 // Randomly generates the computer choice
 function getComputerChoice() {
-    
-    let randomInt = Math.floor(Math.random()* 3);
 
-    if(randomInt == 0) {return "ROCK";} 
-    else if(randomInt == 1) {return "PAPER";} 
-    else{return "SCISSORS";}
+    let randomInt = Math.floor(Math.random()* 3);
+    return ["ROCK", "PAPER", "SCISSORS"][randomInt];
 }
 
 // Plays one round
 function gambleOneRound(computerSelection, playerSelection) {
+
     if(computerSelection == playerSelection) {
         return "That's a draw!"
     } else if(computerSelection == "PAPER" && playerSelection == "SCISSORS" ||
@@ -30,40 +41,43 @@ function gambleOneRound(computerSelection, playerSelection) {
         return "Hmm... looks like you lost this round! " + computerSelection.charAt(0) + computerSelection.slice(1).toLowerCase() + " beats " + playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase() + "."
     } else 
         return "Not a valid answer. In order to participate at this game, please choose from 'Rock', 'Paper' or 'Scissors'."
+
 }
 
-// Plays 5 rounds and counts scores
-function game() {
 
-    for (let i = 1; i <= 5; i++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("Make a choice").toUpperCase();
-        const result = gambleOneRound(computerSelection,playerSelection);
+function game(result) {
 
-        if(result.includes("won")) {
-            yourScore += 1;
-        } else if (result.includes("lost")) {
-            computerScore += 1;
-        } else if (result.includes("Not a valid")) {
-            i -= 1;
-        }
+    let resultsDiv = document.querySelector('.results');
+    resultsDiv.innerHTML += result + "<br>";
 
-        console.log("--------------------------")
-        console.log("Round "+ i + "/" + 5)
-        console.log("The computer choosed: " + computerSelection);
-        console.log("You choosed: " + playerSelection);
-        console.log(gambleOneRound(computerSelection, playerSelection));
-        console.log("Your score is " + yourScore + ".");
-        console.log("The computers score is " + computerScore + ".");
+    if(result.includes("won")) {
+        myScore += 1;
+    } else if (result.includes("lost")) {
+        compScore += 1;
     }
 
-    if(yourScore > computerScore){
-        console.log(`You won the game. The game ended with ${yourScore} / ${computerScore}`)
-    } else if (yourScore < computerScore) {
-        console.log(`You lost the game. The game ended with ${yourScore} / ${computerScore}`)
-    } else {
-        console.log(`That's a draw. The game ended with ${yourScore} / ${computerScore}`)
-    }
-    return "--------------------------\nGame finished."
+    document.querySelector('.myScore').innerHTML = "Your Score: " + myScore;
+    document.querySelector('.compScore').innerHTML = "Computer Score: " + compScore;
 
+    if(myScore == 5) {
+        resultsDiv.innerHTML += "You won the game with " + myScore + " to " + compScore + "!<br>";
+        gameOver = true;
+        endGame();
+    } else if(compScore == 5) {
+        resultsDiv.innerHTML += "You lost the game with " + myScore + " to " + compScore + "!<br>";
+        gameOver = true;
+        endGame();
+    }
+}
+
+
+function endGame() {
+    document.getElementById("btn1").disabled = true;
+    document.getElementById("btn2").disabled = true;
+    document.getElementById("btn3").disabled = true;
+
+    const restartButton = document.querySelector(".restart");
+    restartButton.addEventListener("click", () => {
+        location.reload();
+    })
 }
